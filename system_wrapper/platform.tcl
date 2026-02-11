4eaa74df3ed9d4eb187c96afc0e04c1942dedbad
Mason V <mvari@clemson.edu>
2026-02-11 01:08:05 -0500

    Make project portable: relative paths, .gitignore

---
+++ system_wrapper/platform.tcl
@@ -0,0 +1,13 @@
+platform generate -domains 
+platform generate
+platform active {system_wrapper}
+domain active {domain_ps7_cortexa9_0}
+domain active {zynq_fsbl}
+bsp reload
+bsp reload
+platform generate
+platform generate
+platform generate
+platform active {system_wrapper}
+platform config -updatehw {C:/Users/mumbl/Downloads/Zybo-Z7-20-Pcam-5C-hw.xpr/hw/system_wrapper.xsa}
+platform generate -domains 
